fc6a0d178ed5fafc948708cf254b414909bb1e5d
255617 <>
2018-06-23 00:17:25 +0800

    controller 开发

---
+++ src/main/java/com/lennon/sell/VO/ResultVO.java
@@ -0,0 +1,22 @@
+package com.lennon.sell.VO;
+
+import lombok.Data;
+
+/**
+ * http请求返回的最外层对象
+ */
+@Data
+public class ResultVO<T> {
+    /**
+     * 错误码
+     */
+    private Integer code;
+    /**
+     * 提示信息
+     */
+    private String msg;
+    /**
+     * 具体类型
+     */
+    private T data;
+}
