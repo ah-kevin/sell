ff021668240b6a0026c5d1802d07ced6dadc56e4
255617 <>
2018-06-22 22:49:28 +0800

    商品服务层完成

---
+++ src/main/java/com/lennon/sell/service/ProductService.java
@@ -0,0 +1,24 @@
+package com.lennon.sell.service;
+
+import com.lennon.sell.dataobject.ProductInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ProductService {
+    ProductInfo findOne(String productId);
+
+    /**
+     * 查询所在架商品列表
+     */
+    List<ProductInfo> findUpAll();
+
+    Page<ProductInfo> findAll(Pageable pageable);
+
+    ProductInfo save(ProductInfo productInfo);
+    // 加库存
+
+    // 减库存
+
+}
